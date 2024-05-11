e844940c9c0fe85ef8359b9b5dd845bb23ec5b29
nayisntaround87 <naylasalwahayati@gmail.com>
2024-05-11 10:10:13 +0700

    membuat stack class

---
+++ stackusinglinkedlist.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+// node lass representing a single node in the linked list
+class Node 
+{
+public:
+    int data;
+    Node* next;
+
+    Node() 
+    {
+        next = NULL;
+    }
+};
+
+// stack class
+class Stack 
+{
+    private:
+    Node* top; // pointer to the top node of the stack
+
+    public:
+    Stack() 
+    {
+        
+    }
+
+}
